65b02aca14d1a570f3a9eaa984144dc8aaa273f6
nazeertsm <59368254+nazeertsm@users.noreply.github.com>
2022-05-29 06:11:53 +0530

    Add files via upload

---
+++ 03-Arrays/Arrays_PassByReferebce.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+//printArray function accept the array
+void printArray(int *arr, int n) {
+
+	cout << "in the method " << sizeof(arr) << endl;
+	arr[0] = 100;
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << endl;
+
+	}
+
+}
+
+
+int main() {
+
+//create and initialize the array
+	int arr[] = { 1, 2, 3, 4, 5, 6 };
+
+	//How many element are there in array
+	int n = sizeof(arr) / sizeof(int);// 24 bytes/4 bytes
+
+	cout << "in the main " << sizeof(arr) << endl;
+	/*call the printArray method
+	Arrys are passed by reference  */
+
+	printArray(arr, n);
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << endl;
+
+	}
+
+	return 0;
+}
